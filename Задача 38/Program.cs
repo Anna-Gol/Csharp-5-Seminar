323ca51538fa2dfd2b873183a1c71e43a361477c
Anna Golikova <annagolikovast@gmail.com>
2022-09-05 17:53:38 +0400

    38 done

---
+++ Задача 38/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+string Difference()
+
+{
+    int[] array = {3, 7, 22, 2, 78};
+    int min = array[0];
+    int max = array[0];
+    int numDif = 0;
+ 
+    for (int i = 0; i < array.Length; i++)
+
+        {
+            if (array[i] < min)
+            {
+                 min = array[i];
+            }
+
+            else if (array[i] > max)
+            {
+                max = array[i];
+            }
+
+            numDif = max - min;
+        }
+
+    
+
+    string output = $"Difference betweem {max} and {min} = {numDif}";
+
+    return output;
+
+
+}
+
+Console.WriteLine(Difference());

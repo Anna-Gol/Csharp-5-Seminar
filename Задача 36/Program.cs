83f5558b571235905d14bfcbfb5a13281b991153
Anna Golikova <annagolikovast@gmail.com>
2022-09-05 16:31:24 +0400

    36 done

---
+++ Задача 36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+string sumOfNegPosition()
+{
+    int[] arr = new int [4];
+    int sumNeg = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-99, 100);
+
+        if (i % 2 != 0)
+        {
+            sumNeg += arr[i];
+        }
+
+    }
+    
+    string output = $"Array: {String.Join(", ", arr)}\nSum of numbers with negative positions = {sumNeg}";
+
+    return output;
+
+}
+
+Console.WriteLine(sumOfNegPosition());
